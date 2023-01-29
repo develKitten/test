2416685b1b95bd7cb2238bd3bd71a5a128158d30
crab <55537959+develKitten@users.noreply.github.com>
2023-01-29 22:19:14 +0900

    change files
    yeah

--- v2.js
+++ v2.js
@@ -1,11 +1,26 @@
-var dosbox = '';
-var exeName = 'V2.COM'; 
-var zipUrl = 'V2.zip';
-		
-dosbox = new Dosbox({
-		id: "dosbox",
-		onload: dosbox => dosbox.run(zipUrl, exeName),
-});
-dosbox.ui.overlay.hide();
-dosbox.ui.showLoader();
-dosbox.downloadScript();
+console.clear();
+console.log('Created by Sven Neumann <sven@svencodes.com>');
+
+var dosbox, exeName, zipUrl = '';
+
+document.getElementById('zip')
+	.addEventListener('drop', event => zipUrl = URL.createObjectURL(event.dataTransfer.files[0]));
+document.getElementById('start')
+	.addEventListener('click', event => {
+		exeName = document.getElementById('exe').value;
+		zipUrl =  URL.createObjectURL(document.getElementById('zip').files[0]);
+		if(exeName === '' || zipUrl === '') return alert('Please enter .EXE name and .ZIP file');
+		dosbox = new Dosbox({
+			id: "dosbox",
+			onload: dosbox => dosbox.run(zipUrl, exeName),
+			onrun: (dosbox, app) => {
+				console.log("Starting: '" + app + "'");
+				let fullscreen = document.getElementById('fullscreen');
+				fullscreen.style.display = '';
+				fullscreen.addEventListener('click', event => dosbox.requestFullScreen());
+			}
+		});
+		dosbox.ui.overlay.hide();
+		dosbox.ui.showLoader();
+		dosbox.downloadScript();
+	});
