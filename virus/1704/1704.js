2639bc98a320cdf04bdc7ad275f018a4c55449f9
DESKTOP-0U12703\A <longlngs@naver.com>
2023-01-31 22:37:57 +0900

    Added 1704 virus

---
+++ virus/1704/1704.js
@@ -0,0 +1,13 @@
+Dos(document.getElementById("jsdos"), {
+    wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+    cycles: 1000,
+    autolock: false,
+}).ready(function (fs, main) {
+  fs.extract("https://raw.githack.com/develKitten/test/main/virus/1704/1704.zip").then(function () {
+    main(["-c", "1704.bat"]).then(function (ci) {
+        window.ci = ci;
+    });
+  });
+});
+
+// 
