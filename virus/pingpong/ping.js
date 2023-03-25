ea53c1a0dea03167a6654bccd8d12c1e9e51d874
DESKTOP-0U12703\A <longlngs@naver.com>
2023-03-25 18:09:53 +0900

    마지막 테스트

--- virus/pingpong/ping.js
+++ virus/pingpong/ping.js
@@ -1,12 +1,18 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  const alertClosed = getCookie("alertClosed");
+  if (alertClosed != "true") {
+    alert("이 웹사이트는 PC에서 최적화되어 있습니다. 모바일 환경에서는 일부 기능이 작동하지 않을 수 있습니다.");
+  }
+}
+
 Dos(document.getElementById("jsdos"), {
-  wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
-  cycles: 1000,
-  autolock: false,
+    wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+    cycles: 1000,
+    autolock: false,
 }).ready(function (fs, main) {
   fs.extract("https://raw.githack.com/develKitten/test/main/virus/pingpong/pingpong.zip").then(function () {
     main(["-c", "PINGPONG.bat"]).then(function (ci) {
-      window.ci = ci;
-      checkCookie(); // 게임이 준비되면 쿠키를 체크합니다.
+        window.ci = ci;
     });
   });
 });
@@ -19,32 +25,15 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";" + path;
 }
 
-function showAlert() {
-  const alertBox = document.getElementById("alertBox");
-  if (alertBox) {
-    alertBox.style.display = "block";
-  } else {
-    alert("이 웹사이트는 PC에서 최적화되어 있습니다. 모바일 환경에서는 일부 기능이 작동하지 않을 수 있습니다.");
-  }
-}
-
 function closeAlert() {
   setCookie("alertClosed", "true", 30);
-  const alertBox = document.getElementById("alertBox");
-  if (alertBox) {
-    alertBox.style.display = "none";
-  }
+  document.getElementById("alertBox").style.display = "none";
 }
 
 function checkCookie() {
   const alertClosed = getCookie("alertClosed");
   if (alertClosed != "") {
-    const alertBox = document.getElementById("alertBox");
-    if (alertBox) {
-      alertBox.style.display = "none";
-    }
-  } else {
-    showAlert(); // 쿠키가 없으면 경고창을 보여줍니다.
+    document.getElementById("alertBox").style.display = "none";
   }
 }
 
