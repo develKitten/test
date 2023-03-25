9a278196a865bfaafb2a0c095c767c48b95cc458
DESKTOP-0U12703\A <longlngs@naver.com>
2023-03-25 17:26:17 +0900

    pingpong test

--- virus/pingpong/ping.js
+++ virus/pingpong/ping.js
@@ -14,4 +14,33 @@ Dos(document.getElementById("jsdos"), {
   });
 });
 
-// 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  const expires = "expires="+ d.toUTCString();
+  const path = "path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";" + path;
+}
+
+
+function closeAlert() {
+  setCookie("alertClosed", "true", 30);
+  document.getElementById("alertBox").style.display = "none";
+}
+
+function checkCookie() {
+  const alertClosed = getCookie("alertClosed");
+  if (alertClosed != "") {
+    document.getElementById("alertBox").style.display = "none";
+  }
+}
+
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  if (parts.length == 2) {
+    return parts.pop().split(";").shift();
+  }
+  return "";
+}
+
