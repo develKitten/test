918f20b3462787c2f0448930ea4b341f7e6b59d7
DESKTOP-0U12703\A <longlngs@naver.com>
2023-03-25 18:29:29 +0900

    test

--- virus/pingpong/ping.js
+++ virus/pingpong/ping.js
@@ -12,7 +12,7 @@ Dos(document.getElementById("jsdos"), {
     wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
     cycles: 1000,
     autolock: false,
-}).ready(function (fs, main) {a
+}).ready(function (fs, main) {
   fs.extract("https://raw.githack.com/develKitten/test/main/virus/pingpong/pingpong.zip").then(function () {
     main(["-c", "PINGPONG.bat"]).then(function (ci) {
         window.ci = ci;
