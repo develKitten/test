e8dd83133e979069b1055f3f19fc0816f7d18efa
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-04-23 20:35:29 +0900

    edit

--- virus/ambulance/ambul.js
+++ virus/ambulance/ambul.js
@@ -13,7 +13,7 @@ Dos(document.getElementById("jsdos"), {
     cycles: 1000,
     autolock: false,
 }).ready(function (fs, main) {
-  fs.extract("https://raw.githack.com/develKitten/test/main/virus/ambulance/ambul.zip").then(function () {
+  fs.extract("https://raw.githack.com/develKitten/test/main/virus/ambulance/AMBUL.zip").then(function () {
     main(["-c", "ambul.bat"]).then(function (ci) {
         window.ci = ci;
     });
