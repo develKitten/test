ee71237cefa9cd52020afaf66a53e12085adf927
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-05-24 22:30:37 +0900

    체험 섹션 수정

---
+++ msdos/tutorial.js
@@ -0,0 +1,99 @@
+/* MS-DOS Emulator */
+Dos(document.getElementById("jsdos"), {
+    wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
+    cycles: 1000,
+    autolock: false,
+}).ready(function (fs, main) {
+  fs.extract("https://raw.githack.com/develKitten/test/main/virus/pingpong/pingpong.zip").then(function () {
+    main(["-c", "pingpong.bat"]).then(function (ci) {
+        window.ci = ci;
+    });
+  });
+});
+
+document.querySelector('#jsdos').focus();
+
+
+/* tutorial box */
+let currentMessage = 0;
+let characterName = "가이드";
+const dialogBox = document.createElement('div');
+let interval; // Declare interval variable here to be accessible globally
+
+const messages = [
+    { message: '어서오세요, 처음오신 것을 환영합니다. 여러분들은 40년 전에 사용한 운영체제를 직접 보고 계십니다.', type: 'text' },
+    { message: '\'검은 화면에, 그저 글자만 떠있는데 이게 어떻게 컴퓨터 환경일 수가 있지?\'라고 생각하실 수도 있겠습니다. 하지만 과거 환경은 실제로 이랬습니다. ', type: 'text' },
+    { message: '지금의 컴퓨터 환경은 그림 위주의 환경이어서 초보자들도 쉽게 사용할 수가 있습니다. 하지만, 옛날에는 그림을 여러 개 띄울 컴퓨터 환경이 되지 못했습니다.', type: 'text' },
+    { message: '따라서, 과거 사람들은 이런 환경에서 명령어를 입력하여 컴퓨터를 사용했습니다. 즉, 옛날 운영체제를 완전히 이해하려면 여기서 쓰이던 명령어를 알아야 합니다.', type: 'text'},
+    { message: '... 아, 어려울 것 같다고요? 걱정 마세요, 여기서는 가장 기초적인 명령어 4개만 배우겠습니다. 금방 끝나니 걱정마세요 ^-^!', type:'text'}
+];
+
+function renderDialogBox() {
+    dialogBox.innerHTML = `
+        <div id="dialogTitle">
+            ${characterName}
+        </div>
+        <div id="message" class="textOutput"></div>
+        <div id="dialogFooter">
+            ${(currentMessage === messages.length - 1) ? 'Ok' : 'Next'}
+        </div>
+    `;
+    dialogBox.classList.remove('zoomOut');
+    dialogBox.classList.add('zoomIn');
+    type(messages[currentMessage].message);
+}
+function type(sentence) {
+    let i = 0;
+    const textOutput = document.querySelector(".textOutput");
+    textOutput.innerHTML = "";
+
+    const intervalDuration = 15;
+    clearInterval(interval); // Stop any ongoing text printing
+
+    function printCharacter() {
+        if (i < sentence.length) { // If sentence is not yet fully printed
+            textOutput.innerHTML += sentence[i];
+            i++;
+            setTimeout(printCharacter, intervalDuration);
+        }
+    }
+    printCharacter();
+}
+
+
+function handleClick() {
+    if (currentMessage < messages.length - 1) {
+        currentMessage++;
+        renderDialogBox();
+        console.log("hey");
+    } else {
+        console.log("here");
+        dialogBox.classList.remove('zoomIn');
+        dialogBox.classList.add('zoomOut');
+
+        // Use the 'animationend' event instead of setTimeout to wait for the animation to finish
+        dialogBox.addEventListener('animationend', () => {
+            dialogBox.style.display = "none"; // Hide dialog box after animation ends
+        }, { once: true }); // Make sure the event listener only runs once
+
+        console.log('Dialog Ended');
+    }
+}
+
+window.onload = () => {
+    dialogBox.id = 'dialogBox';
+    document.body.appendChild(dialogBox);
+    renderDialogBox();
+};
+
+document.addEventListener('keydown', (event) => {
+    if ((event.code === 'Enter' || event.code === 'Space') && document.querySelector(".textOutput").innerText === messages[currentMessage].message) {
+        handleClick();
+    }
+});
+
+if ('ontouchstart' in window) {
+    dialogBox.addEventListener('touchstart', handleClick);
+} else {
+    dialogBox.addEventListener('click', handleClick);
+}
---
+++ v3_main.js
@@ -0,0 +1,171 @@
+// -----------------------------------------------
+//                Constant, Variables
+// ------------------------------------------------
+
+// prev-section
+const nextButton = document.querySelector(".next-button");
+const textOutput = document.querySelector(".text-output");
+const cursor = document.querySelector(".cursor");
+const textContainer = document.querySelector(".text-container");
+// container 
+const main = document.querySelector(".main");
+const sub = document.querySelector(".sub");
+const sButton = document.querySelector(".start-button");
+const stars = document.querySelector(".background-stars");
+
+const dialog = [
+  "시간 여행자여, 환영합니다. 지금부터는 90년대의 컴퓨터 세계로의 여정이 시작됩니다.",
+  "그리고, 당신은 그 곳에서 세계를 혼란에 빠뜨렸던 컴퓨터 바이러스들의 존재를 눈으로 보게 될 것입니다.",
+  "하지만 두려워하지 마십시오.<br>여느 컴퓨터가 그렇듯, 옛날에도 컴퓨터를 지켜줬던 '백신'이라는 수호자가 있었으니... <br><br>그 수호자가 여행자님을 보호할 것입니다.",
+  "그럼, 부디 안전하고 즐거운 컴퓨터 여행이 되시길..."
+];
+
+let index = 0;
+let interval;
+
+
+// -----------------------------------------------
+//                Event 처리
+// -----------------------------------------------
+
+type(dialog);
+
+function onNextButtonClick(event) {
+  if (index < dialog.length - 1) {
+    index++;
+    type(dialog);
+  }
+}
+
+function onKeydown(event) {
+  if (event.key === "Enter") {
+    if (index < dialog.length - 1) {
+      index++;
+      type(dialog);
+    }
+  }
+}
+
+function onTouchstart(event) {
+  if (index < dialog.length - 1) {
+    index++;
+    type(dialog);
+  }
+}
+
+function sbuttonClicked(event){
+  fadeOutAndNavigate();
+}
+
+// -----------------------------------------------
+//                Type Function
+// ------------------------------------------------
+
+function type(
+  sentences) {
+  let i = 0;
+
+  if (index < sentences.length) {
+    textOutput.innerHTML = "";
+    nextButton.classList.add("hidden");
+
+    const currentSentence = sentences[index];
+    const intervalDuration = 30;
+
+    // 출력 시작 전에 이벤트 리스너 제거
+    document.removeEventListener("click", onNextButtonClick);
+    document.removeEventListener("keydown", onKeydown);
+    document.removeEventListener("touchstart", onTouchstart);
+
+    interval = setInterval(function () {
+      if (i < currentSentence.length) { // 한 문장이 다 출력되지 않았을 경우
+        // 태그인 경우 처리
+        if (currentSentence[i] === "<") {
+          const closingTagIndex = currentSentence.indexOf(">", i);
+          const tag = currentSentence.substring(i, closingTagIndex + 1);
+          textOutput.innerHTML += tag;
+          i = closingTagIndex + 1;
+        } 
+        else { // 태그가 아닌 일반 문장일 경우
+          textOutput.innerHTML += currentSentence[i];
+          i++;
+        }
+      }  
+      else { // 한 문장이 다 출력되면
+        // 출력 끝난 후에 이벤트 리스너 추가
+        document.addEventListener("click", onNextButtonClick);
+        document.addEventListener("keydown", onKeydown);
+        document.addEventListener("touchstart", onTouchstart);
+
+        // nextButton 다시 보이게 하기
+        clearInterval(interval);
+        nextButton.classList.remove("hidden");
+        
+        // 다른 문장이 남았을 때
+        if (index < sentences.length - 1) {
+          const nextButton = document.querySelector(".next-button");
+          nextButton.style.display = "inline-block";
+        } else { // 마지막 문장일 때
+          const nextButton = document.querySelector(".next-button");
+          
+          // ---------- 이벤트 리스너들 -----------
+          document.addEventListener("click", function () {
+            intro();
+          });
+
+          document.addEventListener("keydown", function (event) {
+            intro();
+
+          });
+
+          document.addEventListener("touchstart", function () {
+            intro();
+          });
+        }
+      }
+    }, intervalDuration);
+  }
+}
+
+function intro(){
+  // prev-section hidden
+  nextButton.classList.add("hidden");
+  textOutput.classList.add("hidden");
+  textContainer.classList.add("hidden");
+
+  const container = document.querySelector('.container');
+  container.classList.add('fadeInUp');
+
+  // print intro
+  main.classList.remove("hidden");
+  sub.classList.remove("hidden");
+  sButton.classList.remove("hidden");
+  stars.classList.remove("hidden");
+
+  sButton.addEventListener("click", sbuttonClicked);
+}
+
+const elements = document.querySelectorAll('.container'); 
+const background = document.querySelector('.background-container');
+
+const fadeOutDuration = 500; 
+const nextPageDelay = 500; 
+
+function fadeOutAndNavigate() {
+  let opacity = 1;
+  const intervalDuration = fadeOutDuration / 100; 
+  const interval = setInterval(() => {
+    opacity -= 0.01; 
+    elements.forEach((element) => {
+      element.style.opacity = opacity;
+      background.style.opacity = opacity;
+    });
+    if (opacity <= 0) { // 투명도가 0이 되면 interval을 멈춥니다.
+      clearInterval(interval); 
+      setTimeout(() => {
+
+        window.location.href = 'msdos/tutorial.html'; 
+      }, nextPageDelay);
+    }
+  }, intervalDuration);
+}
