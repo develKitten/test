433acb181a532fc58795b20c45b893c9d80313ee
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-05-30 23:32:24 +0900

    faster cycles

--- msdos/vir_vac/virus/yankee/yankee.js
+++ msdos/vir_vac/virus/yankee/yankee.js
@@ -1,6 +1,6 @@
 Dos(document.getElementById("jsdos"), {
     wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
-    cycles: 1000,
+    cycles: 3000,
     autolock: false,
 }).ready(function (fs, main) {
   fs.extract("https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/yankee.zip").then(function () {
