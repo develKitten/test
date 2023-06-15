91daf035e0fddd0e2a3b2de95ce5d83b950241da
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-15 19:41:22 +0900

    ㅎㅎ

--- msdos/vir_vac/virus/yankee/yankee.js
+++ msdos/vir_vac/virus/yankee/yankee.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 emulators.pathPrefix = "https://rawcdn.githack.com/develKitten/test/tree/main/msdos/vir_vac/virus/yankee/js-dos";
 Dos(document.getElementById("jsdos"), {}).run('https://rawcdn.githack.com/develKitten/test/57c33f620faa7b03276b23d97f7ac8ca1b20b45a/msdos/vir_vac/virus/yankee/yankee.jsdos');
+=======
+emulators.pathPrefix = "js-dos/";
+Dos(document.getElementById("jsdos"), {}).run("https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/yankee.jsdos");
+>>>>>>> parent of 7fc7602 (yankee 수정)
