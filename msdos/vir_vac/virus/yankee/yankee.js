57c33f620faa7b03276b23d97f7ac8ca1b20b45a
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-15 19:30:44 +0900

    이거 해보고 안되면 하...

--- msdos/vir_vac/virus/yankee/yankee.js
+++ msdos/vir_vac/virus/yankee/yankee.js
@@ -1,3 +1,2 @@
-emulators.pathPrefix = "https://develkitten.github.io/test/main/msdos/vir_vac/virus/yankee/js-dos/";
-Dos(document.getElementById("jsdos"), {
-}).run('https://develkitten.github.io/test/msdos/vir_vac/virus/yankee/yankee.jsdos');
+emulators.pathPrefix = "https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/js-dos/";
+Dos(document.getElementById("jsdos"), {}).run('https://raw.githack.com/develKitten/test/msdos/vir_vac/virus/yankee/yankee.jsdos');
