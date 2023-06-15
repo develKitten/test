f0927bbe913f8a72d7c6066b4d43c38c59ea00e0
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-15 16:44:57 +0900

    yankee

--- msdos/vir_vac/virus/yankee/yankee.js
+++ msdos/vir_vac/virus/yankee/yankee.js
@@ -1,11 +1,3 @@
-Dos(document.getElementById("jsdos"), {
-    wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js",
-    cycles: 3000,
-    autolock: false,
-}).ready(function (fs, main) {
-  fs.extract("https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/yankee.zip").then(function () {
-    main(["-c", "yankee.bat"]).then(function (ci) {
-        window.ci = ci;
-    });
-  });
-});
+emulators.pathPrefix = "/v7/build/releases/latest/js-dos/";
+Dos(document.getElementById("jsdos"))
+    .run("https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/yankee.jsdos");
