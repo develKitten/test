1193c12113c4db1dd853292f2fcdf2de4b1fa4ca
DESKTOP-571MKVN\seclab <longlngs@naver.com>
2023-06-15 19:21:24 +0900

    debugging

--- msdos/vir_vac/virus/yankee/yankee.js
+++ msdos/vir_vac/virus/yankee/yankee.js
@@ -1,4 +1,8 @@
 emulators.pathPrefix = "https://raw.githack.com/develKitten/test/main/msdos/vir_vac/virus/yankee/js-dos/";
 alert("Setting path prefix: " + emulators.pathPrefix);  // pathPrefix 값을 팝업으로 출력
 
-Dos(document.getElementById("jsdos"), {}).run('https://develkitten.github.io/test/msdos/vir_vac/virus/yankee/yankee.jsdos');
+Dos(document.getElementById("jsdos"), {
+  onprogress: function (stage, total, loaded) {
+    alert(stage + ": " + loaded + "/" + total);  // 로딩 상태를 팝업으로 출력
+  }
+}).run('https://develkitten.github.io/test/msdos/vir_vac/virus/yankee/yankee.jsdos');
